1aba49779e39615dfde3e4e36a294ff4921c4fb6
sethpow <sethmadspowelson@gmail.com>
2021-02-23 17:15:59 -0700

    final

--- README.md
+++ README.md
@@ -9,3 +9,6 @@
     - Infura - connect to blockchain without running a node
     - Heroku
     - https://kovan.etherscan.io/
+        - Exchange: 0x1cB6579E060A40BCE0726D798c704A9a68BF8cBc
+        - Migrations: 0x31cf54C15aa670755dFb92d6E54BC0919032Da96
+        - Token: 0x538F45Ba62CFddD22bE4398C160eF33Ea0230d96
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,9 @@
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
 
@@ -9,6 +12,20 @@ module.exports = {
             host: "127.0.0.1",
             port: 7545,
             network_id: "*"
+        },
+        kovan: {
+            provider: function() {
+                // generate wallet from private keys in ganache
+                return new HDWalletProvider(
+                    // private key
+                    privateKeys.split(','),
+                    // url to ethereum node - infura
+                    `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`
+                )
+            },
+            gas: 5000000,
+            gasPrice: 25000000000,
+            network_id: 42
         }
     },
     contracts_directory: './src/contracts/',
